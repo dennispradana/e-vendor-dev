6fe78ae21575ea28fe7e5254e93a6b24331eb03a
dennispradana <dennisoldyatha@gmail.com>
2023-10-27 20:33:46 +0700

    add identitas page

---
+++ src/pages/penyedia/dataPenyedia/addIzinUsaha.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormIzinUsaha from '../../../components/Fragments/FormIzinUsaha';
+
+const AddIzinUsaha = () => {
+  return (
+    <div className="p-6 bg-white border rounded-lg">
+      <FormIzinUsaha />
+    </div>
+  );
+};
+
+export default AddIzinUsaha;
