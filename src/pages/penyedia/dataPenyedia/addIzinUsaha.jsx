e9ba7da51493fb73aaa8e7577209eebbfd23fa08
dennispradana <dennisoldyatha@gmail.com>
2023-10-29 01:35:39 +0700

    add modal file upload

--- src/pages/penyedia/dataPenyedia/addIzinUsaha.jsx
+++ src/pages/penyedia/dataPenyedia/addIzinUsaha.jsx
@@ -3,9 +3,14 @@ import FormIzinUsaha from '../../../components/Fragments/FormIzinUsaha';
 
 const AddIzinUsaha = () => {
   return (
-    <div className="p-6 bg-white border rounded-lg">
-      <FormIzinUsaha />
-    </div>
+    <>
+      <h1 className="my-10 text-xl font-bold text-black capitalize">
+        Tambah Izin Usaha
+      </h1>
+      <div className="py-10 mb-10 border rounded-lg page-padding">
+        <FormIzinUsaha />
+      </div>
+    </>
   );
 };
 
