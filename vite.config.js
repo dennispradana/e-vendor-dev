9e3b9e357db2dba0354eb74c17938648e8b0a102
Tahajuda <tahajuda@tahajuda-X441UV>
2023-11-15 23:55:33 +0700

    modif

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-//  base: '/e-vendor-dev/dist/',
+  // base: 'http://192.168.2.253/coba/',
 })
