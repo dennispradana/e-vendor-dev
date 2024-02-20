be4b3106a00c5098d1a91a55121c85be8e40d432
dennispradana <dennisoldyatha@gmail.com>
2024-02-20 21:43:32 +0700

    add logo navbar

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavAuth from '../Elements/Navbar/NavAuth';
 import NavUser from '../Elements/Navbar/NavUser';
+import Logo from '../../assets/logo.png';
 
 const Navbar = ({ type }) => {
   const handleBack = () => {
@@ -16,8 +17,8 @@ const Navbar = ({ type }) => {
     <nav className="container fixed top-0 z-10 p-4 shadow-xl h-18 bg-slate-600">
       <div className="flex items-center justify-between w-full px-4">
         <button onClick={() => handleBack()}>
-          <div className="px-8 text-xl font-bold text-white uppercase">
-            Logo
+          <div className="p-1 bg-white rounded-sm">
+            <img src={Logo} alt="logo" className="h-10" />
           </div>
         </button>
       </div>
